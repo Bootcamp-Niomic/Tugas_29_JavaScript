3eadcc3b4e851822c7caa977c0e940e9ba927b63
Havid <great_havid@yahoo.co.id>
2019-11-13 11:49:25 +0700

    Tugas 29 JS

---
+++ tugas29.js
@@ -0,0 +1,9 @@
+// ====== Tugas 29 JavaScript =====
+
+function regex() {
+    let data = "Belajar menimba ilmu programming bersama Niomic";
+    let str = new RegExp ("bersama")
+    console.log(str.exec(data))
+   }
+
+   regex()
